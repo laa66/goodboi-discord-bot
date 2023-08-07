ee8751e4cbf0263c2ec63e644d16cbdf7f36bb96
laa66 <max.sclz94@gmail.com>
2023-08-07 23:02:00 +0200

    Initial commit

---
+++ src/main/java/com/laa66/goodboi/GoodboiDiscordBotApplication.java
@@ -0,0 +1,13 @@
+package com.laa66.goodboi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodboiDiscordBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoodboiDiscordBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/laa66/goodboi/GoodboiDiscordBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laa66.goodboi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodboiDiscordBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
