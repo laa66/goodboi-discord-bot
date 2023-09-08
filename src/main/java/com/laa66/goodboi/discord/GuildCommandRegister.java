da5c70b8e8b79e80ea228400e18ae3c5e3d5fe13
laa66 <max.sclz94@gmail.com>
2023-09-08 23:45:47 +0200

    Created readJsonCommand and getJsonPaths, modified buildGuildCommands in GuildCommandRegister

--- src/main/java/com/laa66/goodboi/discord/GuildCommandRegister.java
+++ src/main/java/com/laa66/goodboi/discord/GuildCommandRegister.java
@@ -1,19 +1,30 @@
 package com.laa66.goodboi.discord;
 
+import discord4j.common.JacksonResources;
 import discord4j.discordjson.json.ApplicationCommandRequest;
-import discord4j.discordjson.json.ImmutableApplicationCommandRequest;
 import discord4j.rest.RestClient;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.io.ClassPathResource;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Objects;
 import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
+@Slf4j
+@AllArgsConstructor
 public class GuildCommandRegister {
 
-    RestClient client;
-
-    public GuildCommandRegister(RestClient client) {
-        this.client = client;
-    }
+    private final RestClient client;
+    private final JacksonResources mapper = JacksonResources.create();
 
     public void registerCommands(String devGuildId) {
         Optional<Long> appId = Optional.ofNullable(client.getApplicationId().block());
@@ -24,21 +35,37 @@ public class GuildCommandRegister {
                 .orElseThrow();
     }
 
-    private Flux<ImmutableApplicationCommandRequest> buildGuildCommands() {
-        return Flux.just(
-                ApplicationCommandRequest.builder()
-                        .name("warns")
-                        .description("List of all warns")
-                        .build(),
-                ApplicationCommandRequest.builder()
-                        .name("bans")
-                        .description("List of all bans")
-                        .build(),
-                ApplicationCommandRequest.builder()
-                        .name("rude")
-                        .description("Show 10 most rude users")
-                        .build()
-        );
+    private Flux<ApplicationCommandRequest> buildGuildCommands() {
+        return Flux.fromStream(getJsonPaths()
+                .stream())
+                .flatMap(this::readJsonCommand)
+                .filter(Objects::nonNull);
+    }
+
+    private Mono<ApplicationCommandRequest> readJsonCommand(Path path) {
+        return Mono.fromCallable(() -> {
+            ApplicationCommandRequest commandRequest = null;
+            try (InputStream resourceStream = new ClassPathResource(path.toString()).getInputStream()) {
+                commandRequest = mapper.getObjectMapper()
+                        .readValue(resourceStream, ApplicationCommandRequest.class);
+            } catch (IOException e) {
+                log.error("Error while reading json commands", e);
+            }
+            return commandRequest;
+        });
+    }
+
+    private Set<Path> getJsonPaths() {
+        Set<Path> paths = null;
+        try (Stream<Path> pathStream = Files.list(Path.of("src/main/resources/commands"))) {
+            paths = pathStream
+                    .filter(file -> !Files.isDirectory(file))
+                    .map(path -> Path.of(path.getParent().getFileName() + "/" + path.getFileName()))
+                    .collect(Collectors.toSet());
+        } catch (IOException e) {
+            log.error("Error while reading json paths", e);
+        }
+        return paths;
     }
 
 }
---
+++ src/test/java/com/laa66/goodboi/discord/GuildCommandRegisterUnitTest.java
@@ -0,0 +1,56 @@
+package com.laa66.goodboi.discord;
+
+import discord4j.discordjson.json.ApplicationCommandData;
+import discord4j.rest.RestClient;
+import discord4j.rest.service.ApplicationService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import reactor.core.publisher.Mono;
+
+import java.util.NoSuchElementException;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class GuildCommandRegisterUnitTest {
+
+    @Mock
+    RestClient client;
+
+    @Mock
+    ApplicationService applicationService;
+
+    @Mock
+    ApplicationCommandData commandData;
+
+    @InjectMocks
+    GuildCommandRegister guildCommandRegister;
+
+    @Test
+    void shouldRegisterCommandsAppIdNotNull() {
+        when(client.getApplicationId()).thenReturn(Mono.just(23L));
+        when(client.getApplicationService()).thenReturn(applicationService);
+        when(applicationService.createGuildApplicationCommand(anyLong(), anyLong(), any()))
+                .thenReturn(Mono.just(commandData));
+        guildCommandRegister.registerCommands("232");
+
+        verify(client, times(1)).getApplicationId();
+        verify(client, times(3)).getApplicationService();
+        verify(applicationService, times(3))
+                .createGuildApplicationCommand(eq(23L), eq(232L), argThat(
+                        commandRequest -> commandRequest.name().equals("bans")
+                        || commandRequest.name().equals("warns")
+                        || commandRequest.name().equals("rude")
+                ));
+    }
+
+    @Test
+    void shouldRegisterCommandsAppIdNull() {
+        when(client.getApplicationId()).thenReturn(Mono.empty());
+        assertThrows(NoSuchElementException.class, () -> guildCommandRegister.registerCommands("232"));
+    }
+}
